8f816559c3fd7d1a11fbb5b4d54c553e43b7b6ab
boyelacosta <boyelacosta@gmail.com>
2023-11-29 12:38:25 -0600

    dia1

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,14 @@ import D_envio from "./Pages/D_envio";
 import SingleProduct from "./Pages/SingleProduct";
 import { Routes, Route } from "react-router-dom";
 import M_pago from "./Pages/M_pago";
-
 const App = () => {
   return (
 <Routes>
-<Route path="/Home" element={<Home />} />
+<Route path="/" element={<Home />} index />
 <Route path="/SingleProduct" element={<SingleProduct />} />
 <Route path="/envio" element={<D_envio/>}/>
-<Route path="/Pago" key="Pago" element={<M_pago/>}/>
+<Route path="/Pago" key="/Pago" element={<M_pago/>}/>
 </Routes>
   );
 };
 export default App;
-
